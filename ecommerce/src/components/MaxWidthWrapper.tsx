816401777ae53fa8e028efb5b18b2578d5740107
Hassan Bachir <125279202+hassan-bachir@users.noreply.github.com>
2025-01-05 13:42:18 +0200

    added new component

---
+++ ecommerce/src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,6 @@
+import { cn } from "@/lib/utils"
+
+const MaxWidthWrapper = () => {
+return <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5")}></div>
+}       
+export default MaxWidthWrapper
