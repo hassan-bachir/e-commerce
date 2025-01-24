dcafcc8642c8358df0a978a7d6a916caed5b3b77
Hassan Bachir <125279202+hassan-bachir@users.noreply.github.com>
2025-01-24 08:40:46 +0200

    changed the shadcn ui main color

--- ecommerce/src/components/NavBar.tsx
+++ ecommerce/src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
 
 const NavBar = () => {
   const user = undefined;
+  const isAdmin = false;
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -20,10 +22,66 @@ const NavBar = () => {
                     size: "sm",
                     variant: "ghost",
                   })}
-                ></Link>
+                >
+                  Sign out
+                </Link>
+                {isAdmin ? (
+                  <Link
+                    href="/api/auth/logout"
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    Dashboard ✨
+                  </Link>
+                ) : null}
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
               </>
             ) : (
-              <></>
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign up
+                </Link>
+
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Login
+                </Link>
+
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block" />
+
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden sm:flex items-center gap-1",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ml-1.5 h-5 w-5" />
+                </Link>
+              </>
             )}
           </div>
         </div>
