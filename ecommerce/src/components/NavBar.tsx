59e6345a2172f9947af6976521673374e40b3bb0
Hassan Bachir <125279202+hassan-bachir@users.noreply.github.com>
2025-01-25 11:25:35 +0200

    added authentication

--- ecommerce/src/components/NavBar.tsx
+++ ecommerce/src/components/NavBar.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const NavBar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -13,6 +17,7 @@ const NavBar = () => {
           <Link href="/" className="flex z-40 font-semibold">
             case<span className="text-green-600">cobra</span>
           </Link>
+
           <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
@@ -27,7 +32,7 @@ const NavBar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
@@ -37,7 +42,7 @@ const NavBar = () => {
                   </Link>
                 ) : null}
                 <Link
-                  href="/api/auth/logout"
+                  href="/configure/upload"
                   className={buttonVariants({
                     size: "sm",
                     className: "hidden sm:flex items-center gap-1",
@@ -89,4 +94,5 @@ const NavBar = () => {
     </nav>
   );
 };
-export default NavBar;
+
+export default Navbar;
