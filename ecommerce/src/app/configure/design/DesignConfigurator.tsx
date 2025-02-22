73a750a8bc8815f65677c859120e27d54b509002
Hassan Bachir <125279202+hassan-bachir@users.noreply.github.com>
2025-02-22 21:31:59 +0200

    fixed an annoying bug

--- ecommerce/src/app/configure/design/DesignConfigurator.tsx
+++ ecommerce/src/app/configure/design/DesignConfigurator.tsx
@@ -17,7 +17,8 @@ const DesignConfigurator = ({
   imageDimensions,
 }: DesignConfiguratorProps) => {
   return (
-    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+    // lg:grid-cols-3
+    <div className="relative mt-20 grid grid-cols-1 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
           <AspectRatio
@@ -36,28 +37,27 @@ const DesignConfigurator = ({
           <div
             className={cn(
               "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
-              `bg-blue-950`
+              `bg-zinc-950`
             )}
           />
-
-          <Rnd
-            default={{
-              x: 150,
-              y: 205,
-              height: imageDimensions.height / 4,
-              width: imageDimensions.width / 4,
-            }}
-          >
-            <div className="relative w-full h-full ">
-              <NextImage
-                src={imageUrl}
-                fill
-                alt="your image"
-                className="pointer-events-none"
-              />
-            </div>
-          </Rnd>
         </div>
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+        >
+          <div className="relative w-full h-full ">
+            <NextImage
+              src={imageUrl}
+              fill
+              alt="your image"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
       </div>
     </div>
   );
